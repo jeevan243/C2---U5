e59824b8c370cac5cc5580dd483982d49042c74c
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-03-14 10:46:03 +0530

    data added

--- C2-Evaluation-U5/src/App.jsx
+++ C2-Evaluation-U5/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { Rentals } from "./Components/Rentals/Rentals";
+import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showdata, setshowdata] = useState(true);
 
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      {showdata ? <Rentals /> : <AddHouse />}
+      <button
+        className="toggleForm"
+        onClick={() => {
+          setshowdata(showdata ? false : true);
+        }}
+      >
+        {/* Show text Add House or Show Rentals based on state */}
+        {showdata ? "Add House" : "Show Rentals"}
+
+        <br />
+        <br />
+      </button>
+      {/* Show component based on state */}
+      <br />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ C2-Evaluation-U5/src/Components/AddHouse/AddHouse.jsx
@@ -0,0 +1,138 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Rentals } from "../Rentals/Rentals";
+
+export const AddHouse = () => {
+  const obj = {
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    tenent: "",
+  };
+  const [formdata, setFormdata] = useState(obj);
+  const [getDatadb, setgetDatadb] = useState([]);
+
+  const handleChange = (e) => {
+    const tenent =
+      e.target.id === "married" && e.target.checked === true
+        ? "marred"
+        : "bachelor";
+
+    const { id, value } = e.target;
+
+    if (id === "married" || id === "bachelor") {
+      setFormdata({ ...formdata, tenent: tenent });
+    } else {
+      setFormdata({ ...formdata, [id]: value });
+    }
+  };
+
+  //useeffect
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //server
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8080/houses", formdata).then(() => {
+      alert("Data saved successfully");
+      getData();
+    });
+  };
+
+  //getdata
+
+  const getData = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      setgetDatadb(res.data);
+    });
+  };
+  return (
+    <>
+      <div className="addHouseContainer">
+        <form onSubmit={handleSubmit}>
+          <label>name</label>
+          <input
+            type="text"
+            className="name"
+            id="name"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <label>ownerName</label>
+          <input
+            type="text"
+            className="ownerName"
+            id="ownerName"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <label>address</label>
+          <input
+            type="text"
+            className="address"
+            id="address"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <label>areaCode</label>
+          <input
+            type="text"
+            className="areaCode"
+            id="areaCode"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <label>rent</label>
+          <input
+            type="text"
+            className="rent"
+            id="rent"
+            required
+            onChange={handleChange}
+          />
+          <br />
+          <label>preferredTenant</label>
+          <br />
+          <label>bachelor</label>
+          <input
+            type="checkbox"
+            className="bachelor"
+            id="bachelor"
+            onChange={handleChange}
+          />
+          <br />
+          <label>married</label>
+          <input
+            type="checkbox"
+            className="married"
+            id="married"
+            onChange={handleChange}
+          />
+          <br />
+          <label>image</label>
+          <input
+            type="text"
+            className="image"
+            id="image"
+            onChange={handleChange}
+          />
+          <br />
+          <input className="submitBtn" type="submit" />
+        </form>
+      </div>
+
+      <Rentals getDatadb={getDatadb} />
+    </>
+  );
+};
